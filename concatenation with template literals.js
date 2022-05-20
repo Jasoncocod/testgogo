79b3ac03fc4e52710ae89e5e988cfc07406fd15b
Jasoncocod <lightmandpe@163.com>
2022-05-20 14:21:28 +0800

    改了变量名

--- concatenation with template literals.js
+++ concatenation with template literals.js
@@ -1,7 +1,7 @@
 let str1="JavaScript";
-let str2="fun";
+let str3="fun";
 console.log(`I am writing code in ${str1}`);
-console.log(`Formatting in ${str1} is ${str2}`);
+console.log(`Formatting in ${str1} is ${str3}`);
 let num1=100;
 console.log(--num1);
 console.log(++num1);
